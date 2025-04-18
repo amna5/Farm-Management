7d689667b808c83a1b746a5c3de8999959cfc1db
amna5 <amnamasood469@gmail.com>
2025-04-18 19:11:16 +0800

    Update FarmManagementSystem.java
    update file name

--- FarmManagementSystem.java
+++ FarmManagementSystem.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class FarmManagementSystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Farm farm = new Farm();
